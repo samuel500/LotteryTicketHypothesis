11380881ec72e8005bda2416f5ff850e502dc118
samuel500 <sknoche@fordham.edu>
2019-11-04 12:50:48 +0000

    file org, more experiments

---
+++ layers.py
@@ -0,0 +1,223 @@
+import tensorflow as tf
+from time import time
+import numpy as np
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, InputLayer, Layer, MaxPool2D, AveragePooling2D,\
+    BatchNormalization, Dropout, ReLU, LeakyReLU, Activation
+from tensorflow.keras import Model
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+import tensorflow_probability as tfp
+
+
+
+INIT_M = 5.
+
+
+class LotteryDense(Layer):
+
+    def __init__(self, units, trainable_kernel=False, **kwargs):
+        self.units = units
+        self.trainable_kernel = trainable_kernel
+        super(LotteryDense, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        shape = (input_shape[-1], self.units)
+        M_init = tf.constant_initializer(INIT_M)
+
+
+        self.W = self.add_weight('W', shape=shape, trainable=self.trainable_kernel, initializer=tf.keras.initializers.GlorotNormal())
+        self.W_rec = self.W.numpy()
+        self.std = np.sqrt(2/(sum(shape)))
+      
+        self.M = self.add_weight('M', shape=shape, trainable=True, initializer=M_init)
+
+        super().build(input_shape)
+
+
+    def get_mask(self):
+        # r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
+        # m = tf.math.greater(tf.sigmoid(self.M), r)
+        # m = tf.dtypes.cast(m, tf.int32)
+        m = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.int32)
+
+        return m
+
+    def to_signed_constant(self):
+        new_w = np.full(self.W.shape, -self.std, dtype=np.float32)*(self.W_rec<0)+np.full(self.W.shape, self.std, dtype=np.float32)*(self.W_rec>=0)
+        self.W.assign(new_w)
+
+
+    def call(self, x, training=True, inverse_mask=False, **kwargs):
+        out = None
+        if training:
+            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)\
+                + tf.sigmoid(self.M)\
+                - tf.stop_gradient(tf.sigmoid(self.M)) # Trick to let gradients pass
+            # m = tf.sigmoid(self.M)
+            true_w = tf.math.multiply(mask, self.W)
+
+            tot = np.prod(self.M.shape).astype(np.float32)
+            n_nonz = tf.math.count_nonzero(mask)
+            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32) # Dynamic weight rescaling
+
+
+            out = tf.keras.backend.dot(x, true_w)
+
+
+        else:
+            #r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
+            #m = tf.math.greater(tf.sigmoid(self.M), r)
+            #m = tf.dtypes.cast(m, tf.float32)
+            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)
+
+            #print(tf.math.count_nonzero(m))
+            
+            #m = np.random.binomial(1, tf.sigmoid(self.M), self.M.shape)
+            true_w = tf.math.multiply(mask, self.W)
+
+            tot = np.prod(self.M.shape).astype(np.float32)
+            n_nonz = tf.math.count_nonzero(mask)
+            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32)
+
+
+            out = tf.keras.backend.dot(x, true_w)
+        return out
+
+
+class TrainableDropout(Layer):
+
+    def __init__(self, p):
+        self.p = p
+
+
+    def build(self, input_shape):
+        shape = input_shape[1:]
+        M_init = tf.constant_initializer(7.)
+        self.M = self.add_weight('M', shape=shape, trainable=True, initializer=M_init)
+
+
+    def call(self, x, training=True, **kwargs):
+        out = None
+        if training:
+            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)\
+                + tf.sigmoid(self.M)\
+                - tf.stop_gradient(tf.sigmoid(self.M)) # Trick to let gradients pass
+            # m = tf.sigmoid(self.M)
+
+            tot = np.prod(self.M.shape).astype(np.float32)
+            n_nonz = tf.math.count_nonzero(mask)
+
+
+            out = tf.keras.backend.dot(x, mask)
+
+
+        else:
+            #r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
+            #m = tf.math.greater(tf.sigmoid(self.M), r)
+            #m = tf.dtypes.cast(m, tf.float32)
+            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)
+
+            #print(tf.math.count_nonzero(m))
+            
+            #m = np.random.binomial(1, tf.sigmoid(self.M), self.M.shape)
+
+            tot = np.prod(self.M.shape).astype(np.float32)
+            n_nonz = tf.math.count_nonzero(mask)
+
+            out = tf.keras.backend.dot(x, mask)
+        return out
+
+
+
+
+class LotteryConv2D(Layer):
+
+    def __init__(self, filters, kernel_size, strides=1, padding='VALID', trainable_kernel=False, **kwargs):
+        
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+
+        self.trainable_kernel = trainable_kernel
+
+        super(LotteryConv2D, self).__init__(**kwargs)
+
+
+    def build(self, input_shape):
+        shape = (self.kernel_size, self.kernel_size, input_shape[-1], self.filters)
+
+        M_init = tf.constant_initializer(INIT_M)
+
+        self.W = self.add_weight('W', shape=shape, trainable=self.trainable_kernel, initializer=tf.keras.initializers.GlorotNormal())
+        self.W_rec = self.W.numpy()
+        self.std = np.sqrt(2/(np.prod(shape[:-1])+shape[-1]))
+
+        self.M = self.add_weight('M', shape=shape, trainable=True, initializer=M_init)
+        super().build(input_shape)
+
+
+    def call(self, x, training=True, use_mask=True, inverse_mask=False, **kwargs):
+        if training:
+            if use_mask:
+                mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)\
+                    + tf.sigmoid(self.M)\
+                    - tf.stop_gradient(tf.sigmoid(self.M)) # Trick to let gradients pass
+                # m = tf.sigmoid(self.M)
+                if not inverse_mask:
+                    true_w = tf.math.multiply(mask, self.W)
+                else:
+                    mask = 1-mask
+                    true_w = tf.math.multiply(mask, self.W)
+
+
+                tot = np.prod(self.M.shape).astype(np.float32)
+                n_nonz = tf.math.count_nonzero(mask)
+                true_w *= tf.cast(tot/n_nonz, dtype=tf.float32) # Dynamic weight rescaling
+            else:
+                true_w = self.W
+
+            out = tf.nn.conv2d(
+                x,
+                true_w,
+                self.strides,
+                self.padding
+            )
+
+        else:
+            #r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
+            #m = tf.math.greater(tf.sigmoid(self.M), r)
+            #m = tf.dtypes.cast(m, tf.float32)
+            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)
+
+            #print(tf.math.count_nonzero(m))
+            
+            #m = np.random.binomial(1, tf.sigmoid(self.M), self.M.shape)
+            true_w = tf.math.multiply(mask, self.W)
+
+            tot = np.prod(self.M.shape).astype(np.float32)
+            n_nonz = tf.math.count_nonzero(mask)
+            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32)
+
+            out = tf.nn.conv2d(
+                x,
+                true_w,
+                self.strides,
+                self.padding
+            )
+
+        return out 
+
+    def to_signed_constant(self):
+        new_w = np.full(self.W.shape, -self.std, dtype=np.float32)*(self.W_rec<0)+np.full(self.W.shape, self.std, dtype=np.float32)*(self.W_rec>=0)
+        self.W.assign(new_w)
+
+    def get_mask(self):
+        # r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
+        # m = tf.math.greater(tf.sigmoid(self.M), r)
+        # m = tf.dtypes.cast(m, tf.int32)
+        m = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.int32)
+        return m
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from time import time
 import numpy as np
-from itertools import product
 
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, InputLayer, Layer, MaxPool2D, AveragePooling2D,\
     BatchNormalization, Dropout, ReLU, LeakyReLU, Activation
@@ -10,6 +9,11 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 import tensorflow_probability as tfp
+
+from layers import *
+
+
+#tf.random.set_seed(8)
 
 
 mnist = tf.keras.datasets.mnist
@@ -26,10 +30,20 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = x_train[..., tf.newaxis]
 x_test = x_test[..., tf.newaxis]
 
+n_train = 30000
+#print('n:', n_train)
+x_train1 = x_train #[:n_train]
+y_train1 = y_train #[:n_train]
+x_train2 = x_train #[n_train:]
+y_train2 = y_train #[n_train:]
 
 
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(128)
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(128)
+train_ds1 = tf.data.Dataset.from_tensor_slices((x_train1, y_train1)).shuffle(10000).batch(128)
+
+train_ds2 = tf.data.Dataset.from_tensor_slices((x_train2, y_train2)).shuffle(10000).batch(128)
+
+
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(1024)
 
 
 
@@ -45,164 +59,8 @@ test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
 
 
-class LotteryDense(Layer):
-
-    def __init__(self, units, trainable_kernel=False, **kwargs):
-        self.units = units
-        self.trainable_kernel = trainable_kernel
-        super(LotteryDense, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        shape = (input_shape[-1], self.units)
-        M_init = tf.constant_initializer(1.)
-
-
-        self.W = self.add_weight('W', shape=shape, trainable=self.trainable_kernel, initializer=tf.keras.initializers.GlorotNormal())
-        self.W_rec = self.W.numpy()
-        self.std = np.sqrt(2/(sum(shape)))
-      
-        self.M = self.add_weight('M', shape=shape, trainable=True, initializer=M_init)
-
-        super().build(input_shape)
-
-
-    def get_mask(self):
-        # r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
-        # m = tf.math.greater(tf.sigmoid(self.M), r)
-        # m = tf.dtypes.cast(m, tf.int32)
-        m = tf.cast(tfp.distributions.Bernoulli(probs=tf.nn.sigmoid(self.M)).sample(), dtype=tf.int32)
-
-        return m
-
-    def to_signed_constant(self):
-        new_w = np.full(self.W.shape, -self.std, dtype=np.float32)*(self.W_rec<0)+np.full(self.W.shape, self.std, dtype=np.float32)*(self.W_rec>=0)
-        self.W.assign(new_w)
-
-
-    def call(self, x, training=True, **kwargs):
-        out = None
-        if training:
-            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)\
-                + tf.nn.sigmoid(self.M)\
-                - tf.stop_gradient(tf.nn.sigmoid(self.M)) # Trick to let gradients pass
-            # m = tf.sigmoid(self.M)
-            true_w = tf.math.multiply(mask, self.W)
-
-            tot = np.prod(self.M.shape).astype(np.float32)
-            n_nonz = tf.math.count_nonzero(mask)
-            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32) # Dynamic weight rescaling
-
-
-            out = tf.keras.backend.dot(x, true_w)
-
-
-        else:
-            #r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
-            #m = tf.math.greater(tf.sigmoid(self.M), r)
-            #m = tf.dtypes.cast(m, tf.float32)
-            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.nn.sigmoid(self.M)).sample(), dtype=tf.float32)
-
-            #print(tf.math.count_nonzero(m))
-            
-            #m = np.random.binomial(1, tf.sigmoid(self.M), self.M.shape)
-            true_w = tf.math.multiply(mask, self.W)
-
-            tot = np.prod(self.M.shape).astype(np.float32)
-            n_nonz = tf.math.count_nonzero(mask)
-            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32)
-
-
-            out = tf.keras.backend.dot(x, true_w)
-        return out
-
-
-class LotteryConv2D(Layer):
-
-    def __init__(self, filters, kernel_size, strides=1, padding='VALID', trainable_kernel=False, **kwargs):
-        
-        self.filters = filters
-        self.kernel_size = kernel_size
-        self.strides = strides
-        self.padding = padding
-
-        self.trainable_kernel = trainable_kernel
-
-        super(LotteryConv2D, self).__init__(**kwargs)
-
-
-    def build(self, input_shape):
-
-        shape = (self.kernel_size, self.kernel_size, input_shape[-1], self.filters)
-
-        M_init = tf.constant_initializer(1.)
-
-        self.W = self.add_weight('W', shape=shape, trainable=self.trainable_kernel, initializer=tf.keras.initializers.GlorotNormal())
-        self.W_rec = self.W.numpy()
-        self.std = np.sqrt(2/(np.prod(shape[:-1])+shape[-1]))
-
-        self.M = self.add_weight('M', shape=shape, trainable=True, initializer=M_init)
-        super().build(input_shape)
-
-
-    def call(self, x, training=True, **kwargs):
-        if training:
-            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.sigmoid(self.M)).sample(), dtype=tf.float32)\
-                + tf.nn.sigmoid(self.M)\
-                - tf.stop_gradient(tf.nn.sigmoid(self.M)) # Trick to let gradients pass
-            # m = tf.sigmoid(self.M)
-            true_w = tf.math.multiply(mask, self.W)
-
-            tot = np.prod(self.M.shape).astype(np.float32)
-            n_nonz = tf.math.count_nonzero(mask)
-            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32) # Dynamic weight rescaling
-
-
-            out = tf.nn.conv2d(
-                x,
-                true_w,
-                self.strides,
-                self.padding
-            )
-
-        else:
-            #r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
-            #m = tf.math.greater(tf.sigmoid(self.M), r)
-            #m = tf.dtypes.cast(m, tf.float32)
-            mask = tf.cast(tfp.distributions.Bernoulli(probs=tf.nn.sigmoid(self.M)).sample(), dtype=tf.float32)
-
-            #print(tf.math.count_nonzero(m))
-            
-            #m = np.random.binomial(1, tf.sigmoid(self.M), self.M.shape)
-            true_w = tf.math.multiply(mask, self.W)
-
-            tot = np.prod(self.M.shape).astype(np.float32)
-            n_nonz = tf.math.count_nonzero(mask)
-            true_w *= tf.cast(tot/n_nonz, dtype=tf.float32)
-
-            out = tf.nn.conv2d(
-                x,
-                true_w,
-                self.strides,
-                self.padding
-            )
-
-        return out 
-
-    def to_signed_constant(self):
-        new_w = np.full(self.W.shape, -self.std, dtype=np.float32)*(self.W_rec<0)+np.full(self.W.shape, self.std, dtype=np.float32)*(self.W_rec>=0)
-        self.W.assign(new_w)
-
-    def get_mask(self):
-        # r = tf.random.uniform(shape=self.M.shape, minval=0, maxval=1)
-        # m = tf.math.greater(tf.sigmoid(self.M), r)
-        # m = tf.dtypes.cast(m, tf.int32)
-        m = tf.cast(tfp.distributions.Bernoulli(probs=tf.nn.sigmoid(self.M)).sample(), dtype=tf.int32)
-        return m
-
-
-
 #@tf.function
-def train_step(images, labels, optimizer, trainable_variables):
+def train_step(images, labels, optimizer, trainable_variables, reg=False):
     with tf.GradientTape() as tape:
         predictions = model(images, training=True)
         #kl_loss = -0.5*tf.math.reduce_sum((1 + logvar - tf.square(mu) - tf.exp(logvar)))
@@ -210,9 +68,15 @@ def train_step(images, labels, optimizer, trainable_variables):
         #kl_loss = 0.00012*tf.reduce_mean(kl_loss)
 
         loss = loss_object(labels, predictions)
-        #loss += kl_loss
-        #print(loss)
-        #print(kl_loss)
+
+        if reg:
+            reg_loss = 0
+            for layer in model.layers:
+                if type(layer) in {LotteryDense, LotteryConv2D}:
+                    reg_loss += tf.reduce_sum(layer.M)
+            reg_loss *= 8e-7
+            loss += reg_loss
+
     gradients = tape.gradient(loss, trainable_variables)
     optimizer.apply_gradients(zip(gradients, trainable_variables))
 
@@ -233,7 +97,7 @@ def test_step(images, labels):
 def get_all_masks(layers):
     masks = []
     for layer in layers:
-        if type(layer) in {LotteryDense, LotteryConv2D}:
+        if type(layer) in {LotteryDense, LotteryConv2D, TrainableDropout}:
             masks.append(layer.M)
     return masks
 
@@ -249,7 +113,7 @@ def print_p_pruned(layers):
     tot_w = 0
     tot_m = 0
     for i, l in enumerate(layers):
-        if type(l) is LotteryDense or type(l) is LotteryConv2D:
+        if type(l) in{LotteryDense, LotteryConv2D, TrainableDropout}:
             tot = np.prod(l.M.shape)
             tot_w += tot
             mask = l.get_mask()
@@ -262,18 +126,20 @@ def print_p_pruned(layers):
     print('Tot p pruned:', 1-tot_m/tot_w)
 
 
+lott_t = True
+
 layers = [
     InputLayer(input_shape=(28, 28, 1)),
-    LotteryConv2D(16, 3, strides=2),
+    LotteryConv2D(32, 3, strides=2, trainable_kernel=lott_t),
     LeakyReLU(),
 
-    LotteryConv2D(32, 3, strides=2),
+    LotteryConv2D(64, 3, strides=2, trainable_kernel=lott_t),
     LeakyReLU(),
 
-    LotteryConv2D(64, 3, strides=1),
+    LotteryConv2D(128, 3, strides=1, trainable_kernel=lott_t),
     LeakyReLU(),
 
-    LotteryConv2D(128, 3, strides=2),
+    LotteryConv2D(256, 3, strides=2, trainable_kernel=lott_t),
     LeakyReLU(),
 
     Flatten(),
@@ -282,7 +148,7 @@ layers = [
     # LotteryDense(256),
     # LeakyReLU(),
     # Dropout(0.3),
-    LotteryDense(10),
+    LotteryDense(10, trainable_kernel=lott_t),
     Activation('softmax')
 ]
 
@@ -295,29 +161,44 @@ model.summary()
 
 
 
-
 if __name__=='__main__':
     kernel_optimizer = tf.keras.optimizers.Adam()
     mask_optimizer = tf.keras.optimizers.SGD(100, momentum=0.9)
 
+    switch = 20
 
-    EPOCHS = 200
+    EPOCHS = 500
 
     for epoch in range(EPOCHS):
-        
-        # get_all_masks(model.layers)
-        # raise
-        if epoch==0:
-            print('ttt')
-            for i, l in enumerate(model.layers):
-                if type(l) is LotteryDense or type(l) is LotteryConv2D:
-                    l.to_signed_constant()
 
-
+        # if epoch==0:
+        #     print('ttt')
+        #     for i, l in enumerate(model.layers):
+        #         if type(l) is LotteryDense or type(l) is LotteryConv2D:
+        #             l.to_signed_constant()
 
         st = time()
-        for images, labels in train_ds:
-            train_step(images, labels, get_all_masks(model.layers))
+
+        # if epoch < switch:
+
+
+        for i, (images1, labels1) in enumerate(tqdm(train_ds1)):
+            # train_step(images1, labels1, kernel_optimizer, get_all_kernels(model.layers)) # DropConnect! http://yann.lecun.com/exdb/publis/pdf/wan-icml-13.pdf
+            train_step(images1, labels1, mask_optimizer, get_all_masks(model.layers), reg=True)
+            # if epoch<3:
+            #     train_step(images1, labels1, mask_optimizer, model.trainable_variables, reg=True)
+            # else:
+            #     train_step(images1, labels1, mask_optimizer, model.trainable_variables, reg=True, )
+
+
+
+        # for i, (images2, labels2) in enumerate(tqdm(train_ds2)):
+        #     train_step(images2, labels2, mask_optimizer, get_all_masks(model.layers), reg=True)
+
+        # else:
+        #     for i, (images2, labels2) in enumerate(tqdm(train_ds2)):
+
+        #         train_step(images2, labels2, mask_optimizer, get_all_masks(model.layers), reg=True)
 
         print_p_pruned(model.layers)
 
@@ -328,10 +209,10 @@ if __name__=='__main__':
 
         template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
         print(template.format(epoch+1,
-                            round(float(train_loss.result()), 4),
-                            round(float(train_accuracy.result()*100), 3),
-                            round(float(test_loss.result()), 4),
-                            round(float(test_accuracy.result()*100), 2)))
+                round(float(train_loss.result()), 4),
+                round(float(train_accuracy.result()*100), 3),
+                round(float(test_loss.result()), 4),
+                round(float(test_accuracy.result()*100), 2)))
 
         # Reset the metrics for the next epoch
         train_loss.reset_states()
